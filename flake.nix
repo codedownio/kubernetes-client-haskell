78da7229ee8e09648a9a0ea25a806473b007354e
thomasjm <tom@codedown.io>
2024-06-13 23:47:59 -0700

    Fix a bound in all versions

--- flake.nix
+++ flake.nix
@@ -17,14 +17,19 @@
 
       in {
         packages = rec {
-          default = generate;
-
           generate = pkgs.writeShellScriptBin "generate-kubernetes-client-core.sh" ''
             export KUBERNETES_VERSION="$1"
             PACKAGE_VERSION="$2"
             out="kubernetes-$KUBERNETES_VERSION"
+
+            # Generate
             ${pkgs.bash}/bin/bash "${gen}/openapi/haskell.sh" "$out" settings
+
+            # Fill in the package version
             ${pkgs.gnused}/bin/sed -i "s/^version:\s*\(.*\)/version:        $PACKAGE_VERSION/" "$out/kubernetes-client-core.cabal"
+
+            # Fix a bound
+            ${pkgs.gnused}/bin/sed -i 's/\(http-api-data >= 0.3.4 &&\) <0.6/\1 <0.7/' "$out/kubernetes-client-core.cabal"
           '';
         };
       });
